5722fcd1c7986cd32281e2af194743164c91e095
jtmb <james.branco@gmail.com>
2022-01-06 09:32:54 -0500

    initiala commit

---
+++ provision-enviorment.sh
@@ -0,0 +1,60 @@
+# This script installs basic requirements for a new linux machine in my enviorment.
+# Each line executes a different command.
+#          Made by: James Branco (JTMB)
+
+
+    # Update Host OS
+
+        # Updates and installs OS then cleans leftover files (linux debian)
+            apt-get update
+            apt-get upgrade -y
+            apt-get dist-upgrade -y
+            apt-get autoremove -y
+            apt-get autoclean -y
+    
+    # Install Packages
+
+        # Install net tools
+            sudo apt install net-tools
+        # Install IP2
+            sudo apt install ip2
+
+    # Install OpenSSH
+
+        # Installs , enables openSSH , forwards required ports (22, 2002/tcp)
+            sudo apt install openssh-server
+            sudo ufw allow ssh
+            sudo ufw allow 2002/tcp
+        
+        # Move SSH Config to default SSH config location and restart SSH service
+            sudo mv /home/james/provision-new-evn/sshd_config /etc/ssh/sshd_config
+            systemctl restart sshd
+        
+
+    # Install Docker
+        
+        # Remove any old versons present
+            sudo apt-get remove docker docker-engine docker.io containerd runc
+            
+
+        # setup docker repo
+            sudo apt-get install \
+                ca-certificates \
+                curl \
+                gnupg \
+                lsb-release
+        
+        # Add Docker’s official GPG key
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+        # Set up the stable repository
+            echo \
+            "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+            $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+        # Install Docker Engine
+            sudo apt-get update
+            sudo apt-get install docker-ce docker-ce-cli containerd.io
+   
+    # Get IPV4 address
+        ifconfig | grep 'inet 192.168.'
