c8e7b6107f3c7207caf25a08bdd895c2fd90c387
jtmb <86915618+jtmb@users.noreply.github.com>
2022-01-08 16:41:24 -0500

    Update virtualbox.sh

--- virtualbox.sh
+++ virtualbox.sh
@@ -4,9 +4,9 @@ sudo apt purge virtualbox
 
 # Build Virtualbox
 sudo apt-get update
-sudo apt install virtualbox
-sudo apt install virtualbox-ext-pack
-sudo apt-get install virtualbox-dkms
-sudo dpkg-reconfigure virtualbox-dkms 
+sudo apt install virtualbox -y
+sudo apt install virtualbox-ext-pack -y
+sudo apt-get install virtualbox-dkms -y
+sudo dpkg-reconfigure virtualbox-dkms
 sudo dpkg-reconfigure virtualbox
-sudo apt-get install linux-headers-generic
+sudo apt-get install linux-headers-generic -y
