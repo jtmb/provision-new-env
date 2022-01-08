9ebe0ef3857406168ee6c760a2bebac48f7f24e6
jtmb <86915618+jtmb@users.noreply.github.com>
2022-01-08 16:16:56 -0500

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,22 @@
+#houseclean
+sudo apt-get autoclean # only removes files that cannot be downloaded anymore (obsolete)
+sudo apt-get clean
+
+#refresh
+sudo apt-get update #resync package index
+sudo apt-get upgrade #newest versions of all packages, update must be run first
+
+#would upgrade you to the latest kernel in the repositories
+#dist-upgrade is also able to remove existing packages if required
+sudo apt-get dist-upgrade
+
+# fix Broken packages -f 
+sudo apt-get -f install
+sudo dpkg --configure -a
+
+# Remove lock
+# If you are absolutely sure you do not have another upgrade process running.
+# Locked dpkg - only if sure you are not running another update.
+
+sudo rm /var/lib/dpkg/lock
+sudo dpkg --configure -a 
