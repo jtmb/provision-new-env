76ca88aa89d2aa7806ae6b1ead6b21f1cbc5c9e7
jtmb <86915618+jtmb@users.noreply.github.com>
2022-01-10 14:29:30 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,18 +1,18 @@
 #houseclean
-sudo apt-get autoclean # only removes files that cannot be downloaded anymore (obsolete)
-sudo apt-get clean
+sudo apt-get autoclean -y # only removes files that cannot be downloaded anymore (obsolete)
+sudo apt-get clean -y
 
 #refresh
-sudo apt-get update #resync package index
-sudo apt-get upgrade #newest versions of all packages, update must be run first
+sudo apt-get update -y #resync package index
+sudo apt-get upgrade -y #newest versions of all packages, update must be run first
 
 #would upgrade you to the latest kernel in the repositories
 #dist-upgrade is also able to remove existing packages if required
-sudo apt-get dist-upgrade
+sudo apt-get dist-upgrade -y
 
 # fix Broken packages -f 
-sudo apt-get -f install
-sudo dpkg --configure -a
+sudo apt-get -f install -y
+sudo dpkg --configure -a -y
 
 # Remove lock
 # If you are absolutely sure you do not have another upgrade process running.
